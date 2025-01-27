eaf9faa958be3b2203b1a2008a07030cbcbd2674
Moon Sahu <100657272+Rishmo@users.noreply.github.com>
2025-01-27 22:52:03 +0530

    Add files via upload

---
+++ taking_input.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+
+using namespace std;
+
+//int main(){
+//	int var1,var2,var3;
+//	cout<<"Please enter first and second value: ";
+//	cin>>var1>>var2;
+//	var3=var1+var2;
+//	cout<<"Sum of two numbers is: " << var3;
+//	return 0;
+//}
+
+int main(){
+	int var1,var2, sum;
+	cout<<"Enter two values:"<<endl;
+	cout<<"First value: ";
+	cin>>var1;
+	cout<<"\nSecond value: ";
+	cin>>var2;
+	sum=var1+var2;
+	cout<<"\nSum is: "<<sum;
+	return 0;
+}
