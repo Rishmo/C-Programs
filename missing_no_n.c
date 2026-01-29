5ad0bcf158eb957aed59aeaa0c41361099bd7992
Moon Sahu <100657272+Rishmo@users.noreply.github.com>
2026-01-29 19:00:12 +0530

    Missing Numbers.

---
+++ missing_no_n.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main(){
+	int x,y,n,arr[],i,missing;
+	x=1;
+	y=arr[0];
+	printf("Enter upto how many number you want to print: ");
+	scanf("%d",&n);
+	for(i=1;i<=n;i++){
+		scanf("%d",&arr[i]);
+	}
+}
